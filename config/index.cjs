e1af98a5ef988c98c0f63b9ed0dd36f989237d8f
Lonely-flyer <lonelyflyer@163.com>
2024-04-25 21:51:16 +0800

    feat: 改为国际时间判断使用哪个模板

--- config/index.cjs
+++ config/index.cjs
@@ -6,9 +6,37 @@
  * 自行添加属性，以支持更多个性化功能
  */
 
+/** 
+ * 国际时间对应北京时间
+ * 16:00 - 00:00
+ * 17:00 - 01:00
+ * 18:00 - 02:00
+ * 19:00 - 03:00
+ * 20:00 - 04:00
+ * 21:00 - 05:00
+ * 22:00 - 06:00
+ * 23:00 - 07:00
+ * 0:00 - 8:00
+ * 1:00 - 9:00
+ * 2:00 - 10:00
+ * 3:00 - 11:00
+ * 4:00 - 12:00
+ * 5:00 - 13:00
+ * 6:00 - 14:00
+ * 7:00 - 15:00
+ * 8:00 - 16:00
+ * 9:00 - 17:00
+ * 10:00 - 18:00
+ * 11:00 - 19:00
+ * 12:00 - 20:00
+ * 13:00 - 21:00
+ * 14:00 - 22:00
+ * 15:00 - 23:00
+*/
+
 function getConfig() {
-	// 判断当前是上午还是晚上
-	const isMorning = new Date().getHours() < 12 || new Date().getHours() >= 23;
+	// 判断当前是上午还是晚上 国际时间
+	const isMorning = new Date().getHours() < 2 || new Date().getHours() >= 16;
 	console.log('isMorning: ', isMorning);
 
 	const USER_CONFIG = {
@@ -22,34 +50,34 @@ function getConfig() {
 		CITY: "贵阳",
 
 		USERS: [
-			{
-				// 想要发送的人的名字
-				name: "陈皮",
-				// 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-				id: "oVm8968BlldCbEbr6sf0RouaCu54", // oVm8968BlldCbEbr6sf0RouaCu54
-				// 使用微信测试号：你想对他发送的模板消息的模板ID
-				useTemplateId: isMorning ? "xlSq7uhZLhlUr6yCa6f-v2VwTzUsKBwEIPPj2O1Vizk" : "k724cnSPLEZEN2IdufhuxuOHFx9Ya8fut-afxKDD8mg",
-				// 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-				horoscopeDate: "10-24",
-				festivals: [
-					// 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-					{
-						type: "*生日",
-						name: "陈皮",
-						year: "2024",
-						date: "10-24",
-					},
-					// 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-					{
-						type: "生日",
-						name: "陈皮",
-						year: "2024",
-						date: "10-24",
-					},
-				],
-				// 我们在一起已经有xxxx天了的配置
-				// customizedDateList: [],
-			},
+			// {
+			// 	// 想要发送的人的名字
+			// 	name: "陈皮",
+			// 	// 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+			// 	id: "oVm8968BlldCbEbr6sf0RouaCu54", // oVm8968BlldCbEbr6sf0RouaCu54
+			// 	// 使用微信测试号：你想对他发送的模板消息的模板ID
+			// 	useTemplateId: isMorning ? "xlSq7uhZLhlUr6yCa6f-v2VwTzUsKBwEIPPj2O1Vizk" : "k724cnSPLEZEN2IdufhuxuOHFx9Ya8fut-afxKDD8mg",
+			// 	// 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+			// 	horoscopeDate: "10-24",
+			// 	festivals: [
+			// 		// 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+			// 		{
+			// 			type: "*生日",
+			// 			name: "陈皮",
+			// 			year: "2024",
+			// 			date: "10-24",
+			// 		},
+			// 		// 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+			// 		{
+			// 			type: "生日",
+			// 			name: "陈皮",
+			// 			year: "2024",
+			// 			date: "10-24",
+			// 		},
+			// 	],
+			// 	// 我们在一起已经有xxxx天了的配置
+			// 	// customizedDateList: [],
+			// },
 			{
 				// 想要发送的人的名字
 				name: "陈皮",
